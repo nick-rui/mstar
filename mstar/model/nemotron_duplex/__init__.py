165f6a312b34e819001c9c22d06ddfde6cf3151f
merceod <atj10@stanford.edu>
2026-09-20 18:22:54 -0700

    Keep the duplex package import lazy

--- mstar/model/nemotron_duplex/__init__.py
+++ mstar/model/nemotron_duplex/__init__.py
@@ -1,4 +1,1 @@
-"""NVIDIA NemotronLabs VoiceChat-11B (duplex speech-to-speech) support for M*."""
-from mstar.model.nemotron_duplex.nemotron_duplex_model import NemotronDuplexModel
-
-__all__ = ["NemotronDuplexModel"]
+"""NVIDIA NemotronLabs VoiceChat-11B (full-duplex speech-to-speech) support for M*."""
